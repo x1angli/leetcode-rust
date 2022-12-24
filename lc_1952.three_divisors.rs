c08a366cfd86a315e3edc0ab81bfc10e8ef4b4c4
Xiang Li <github@x1ang.li>
2022-12-23 23:41:01 -0800

    Create lc_1952.three_divisors.rs

---
+++ lc_1952.three_divisors.rs
@@ -0,0 +1,18 @@
+impl Solution {
+    pub fn is_three(n: i32) -> bool {
+        let mut cnt:i8 = 2;
+        let mut i:i32 = 2;
+        while i * i < n {
+            if n % i == 0 {
+                cnt += 2;
+            }
+            i += 1
+        }
+        if i * i == n {
+            cnt += 1
+        }
+
+        let res:bool = (cnt == 3);
+        return (res)
+    }
+}
